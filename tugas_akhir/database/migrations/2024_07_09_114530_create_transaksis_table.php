f6c05ede18e73c619f7a6d1709531f186a1a7495
risqi21 <risqi121591@gmail.com>
2024-07-09 18:58:04 +0700

    menambah databes

---
+++ tugas_akhir/database/migrations/2024_07_09_114530_create_transaksis_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id('id_transaksi');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_barang');
+            $table->integer('jumlah');
+            $table->decimal('total_harga', 10, 2);
+            $table->string('metode_pembayaran');
+            $table->string('pengiriman');
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_barang')->references('id')->on('barangs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
