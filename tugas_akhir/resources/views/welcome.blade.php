fd0d55c1936d20f6d4c98429b126740b94b4c483
esterwin <esterwinoto678@gmail.com>
2024-07-09 19:23:59 +0700

    upload projek baru

--- tugas_akhir/resources/views/welcome.blade.php
+++ tugas_akhir/resources/views/welcome.blade.php
@@ -7,9 +7,40 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <center><h1>Selamat datang dikelompok 5</h1></center>
-    <center><h5>Ngodingnya disini guys</h5></center>
-        menambahh lagi
-    <center><img src="img/foto.jpg" alt="" height="90px" width="90px"></center>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="../../../public/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="">
+            <h1>Member Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" require>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" require>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me </label>
+                <a href="#">Forgot password?</a>
+            </div>
+
+            <button type="submit" class="btn">Sign in</button>
+
+            <div class="register-link">
+                <p>Don't have an account? <a href="#">Sign In Here!</a></p>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
